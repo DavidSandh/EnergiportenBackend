9fd1458bb22b6982f578fb5ac4e70f4a2395151f
devmatte <mattejonsson@hotmail.se>
2018-04-24 12:26:56 +0200

    User form first prototype

---
+++ Energiporten/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult IndexRegister()
+        {
+            return View();
+        }
+
+        public ActionResult ShowPersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult ShowBrfSelect()
+        {
+            return View();
+        }
+    }
+}

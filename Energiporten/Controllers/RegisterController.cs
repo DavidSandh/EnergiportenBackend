12076f77b34c263f17eb26a2a666d99e1af17b11
devmatte <mattejonsson@hotmail.se>
2018-04-25 10:05:30 +0200

    User form folder change

--- Energiporten/Controllers/RegisterController.cs
+++ Energiporten/Controllers/RegisterController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication3.Controllers
+namespace Energiporten.Controllers
 {
     public class RegisterController : Controller
     {

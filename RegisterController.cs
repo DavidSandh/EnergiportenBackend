2f1e21f3f0e60fb02c7e5f3129e5a38fe3fdcba4
Sebastian Andersson <customizelol@gmail.com>
2018-05-10 10:15:33 +0200

    Add files via upload
    Controller som hanterar Building, Property och Energy.

---
+++ RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Energiporten.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult IndexRegister()
+        {
+            return View();
+        }
+
+        public ActionResult ShowPersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult ShowBrfSelect()
+        {
+            return View();
+        }
+
+        public ActionResult ShowPropertyInfo()
+        {
+            return View();
+        }
+
+        public ActionResult ShowBuildingInfo()
+        {
+            return View();
+        }
+
+        public ActionResult ShowEnergyInfo()
+        {
+            return View();
+        }
+    }
+}
